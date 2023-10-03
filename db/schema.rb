e9cc9b2ed8fa8beac4907bb126324be665e082e9
kaya <kaaya.xx.x@gmail.com>
2023-10-03 11:14:24 +0900

    css適用

--- db/schema.rb
+++ db/schema.rb
@@ -1,8 +1,11 @@
-ActiveRecord::Schema.define(version: 2023_10_02_131432) do
+ActiveRecord::Schema.define(version: 2023_10_02_182412) do
+
+  # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "posts", force: :cascade do |t|
     t.text "content"
+    t.datetime "time"
   end
 
 end
